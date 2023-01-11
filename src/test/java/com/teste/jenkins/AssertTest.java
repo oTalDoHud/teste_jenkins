afbe42316fc2beef60f823a74652c1baebd0f30d
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-01-11 11:26:50 -0300

    Adicionei JUnit

---
+++ src/test/java/com/teste/jenkins/AssertTest.java
@@ -0,0 +1,11 @@
+package com.teste.jenkins;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
